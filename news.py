47e0db9e3ed02b64cfe73a4f93862635d1e9a20c
ankurchauhan21 <ankurch@ankurch-macbookpro.roam.corp.google.com>
2015-04-25 22:13:19 -0700

    updated news reading

---
+++ news.py
@@ -0,0 +1,33 @@
+from topics import Topics
+
+class News(object):
+  def get_news(self, topics):
+    stories = ""
+    for topic in topics:
+      stories += "In the World of " + topic.upper() + "\n\n"
+      stories += self.news[topic] + "\n\n"
+    return stories
+
+  def __init__(self):
+    self.news = {}
+    self.TOPICS = Topics().TOPICS
+    for topic in self.TOPICS:
+      self.news[topic] = self._get_news(topic)
+
+  def _get_news(self, topic):
+    file_path = 'news/' + topic + '.txt'
+    print ("Reading news from " + file_path)
+    f = open(file_path, 'r')
+    news = f.read()
+    f.close()
+    return news
+
+  def print_news(self):
+    for (topic, story) in self.news.items():
+      print (topic + " news\n")
+      print (story)
+      
+#news = News()
+#print news.get_news(['science', 'sports'])
+#print news.get_news(['sports'])
+#print news.get_news(['technology', 'science'])
--- sender.py
+++ sender.py
@@ -1,5 +1,6 @@
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from news import News
 
 import smtplib
 
@@ -7,15 +8,15 @@ class Sender(object):
   def send_news(self, user_email, user_topics):
     subject = 'Welcome to Janela - window to the world!!!'
     body = 'Your morning digest from Code for Cuba Hackathon\n\n'
-    for user_topic in user_topics:
-      body += user_topic + '\n'
-    print ('Sending news to ' + user_email)
+    body += self.news.get_news(user_topics)
+    print ('Sending news to ' + user_email + " for topics " + str(user_topics))
     self._send_email(user_email, subject, body)
 
   def __init__(self):
     self.username = 'news.janela'
     self.password = 'windowcuba'
     self.fromaddr = 'news.janela@gmail.com'
+    self.news = News()
 
   def _send_email(self, user, subject, body):
     msg = MIMEMultipart()
