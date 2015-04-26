3117433375f36665eacab1522f0ec120d630bc73
ankurchauhan21 <ankurch@ankurch-macbookpro.roam.corp.google.com>
2015-04-25 21:35:54 -0700

    updated main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sender import Sender
 from receiver import Receiver
 from time import sleep
 from users import Users
+from users import Users
 
 def main():
   users = Users()
@@ -9,8 +10,10 @@ def main():
   sender = Sender()
   while True:
     receiver.read_emails(users)
-    for (user_email, user_topics) in users.users_map.items():
-      sender.send_news(user_email, list(user_topics))
+    for user in users.users_map.values():
+      if user.modified:
+        sender.send_news(user.email, list(user.topics_set))
+        user.modified = False
     print ('Sleeping')
     sleep(20)
     print ('Waking up')
